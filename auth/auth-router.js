a55a61b0ca319f9418b107331de1c72e90551ef7
Adam <adamcpenman@gmail.com>
2020-01-18 11:44:42 -0600

    guided lecture code

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs")
 const express = require("express")
 const usersModel = require("../users/users-model")
+const restricted = require("../middleware/restricted")
 
 const router = express.Router()
 
@@ -24,6 +25,10 @@ router.post("/login", async (req, res, next) => {
     const passwordValid = await bcrypt.compare(password, user.password)
 
     if (user && passwordValid) {
+      //stores the user data in current session, 
+      //so it persists between requests
+      req.session.user = user
+
       res.status(200).json({
         message: `Welcome ${user.username}!`,
       })
@@ -46,5 +51,31 @@ router.get("/protected", async (req, res, next) => {
     next(err)
   }
 })
+
+router.get("/logout", restricted(), (req, res,next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      next(err)
+    } else {
+      res.json({
+        message: "BYE FOREVER"
+      })
+    }
+  })
+})
+
+// router.get("/logout", restricted(), (req, res, next) => {
+//   // deletes the session on the server, but not the client's cookie.
+//   // we can't force the client to delete the cookie, it just becomes useless to them.
+//   req.session.destroy((err) => {
+//     if (err) {
+//       next(err)
+//     } else {
+//       res.json({
+//         message: "You are logged out",
+//       })
+//     }
+//   })
+// })
 
 module.exports = router
